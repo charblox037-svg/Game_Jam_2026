94b23fccafcb45ecc961fad31760c7c8fa9445e6
charblox037-svg <charblox037@gmail.com>
2026-06-17 11:06:18 +0100

    Latest Update

--- Project_Formian/Assets/Scripts/DropPoint.cs
+++ Project_Formian/Assets/Scripts/DropPoint.cs
@@ -1,18 +1,38 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class DropPoint : MonoBehaviour
 {
 
-    private object dropPointObject; // Reference to the drop point object
+    public GameObject dropPointObject; // Reference to the drop point object
+
+    public float spawnInterval = 5f; // Interval between spawns in seconds
+    private float spawnTimer; // Timer to track spawn intervals
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnTimer = spawnInterval; // Initialize the spawn timer
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private IEnumerable SpawnObjectsOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval); // Wait for the specified interval
+            SpawnObject(); // Call the spawn method to create objects
+        }
+    }
+
+    private void SpawnObject()
+    {
+        Instantiate(dropPointObject, transform.position, Quaternion.identity); // Instantiate the drop point object at the current position
     }
 }
